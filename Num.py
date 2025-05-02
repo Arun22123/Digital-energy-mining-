0ad321d4fd222554c023075e8d7e58c907f5da27
Arun22123 <125869921+Arun22123@users.noreply.github.com>
2025-05-03 03:06:49 +0530

    Num.py

---
+++ Num.py
@@ -0,0 +1,14 @@
+from time import time
+from number import number
+import phonenumbers 
+from phonenumbers import timezone,geocoder,carrier
+number =input("917004755346: ")
+phone =phonenumbers.parse(number)
+time =timezone.time_zone_for_number(phone)
+car =carrier.name_for_number(phone, "en")
+reg =geocoder.ddescriptions_for_number(phone, "en")
+
+print(phone)
+print(time)
+print(car)
+print(reg)
